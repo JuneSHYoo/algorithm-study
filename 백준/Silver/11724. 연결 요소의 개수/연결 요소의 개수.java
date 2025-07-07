498bdf2dc5f187c35531af647185ae77d0d02943
JuneYoo <juneyo0823@naver.com>
2025-07-08 03:26:50 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 912 ms, Memory: 282940 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    static int N,M;
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] input = sToIArr(br.readLine());
+        N = input[0];
+        M = input[1];
+        arr = new ArrayList[N+1];
+        visited = new boolean[N+1];
+
+        for(int i=1 ; i <= N ; i ++){
+            arr[i] = new ArrayList<>();
+        }
+
+        for(int i=0 ; i < M ; i++){
+            int[] link = sToIArr(br.readLine());
+            arr[link[0]].add(link[1]);
+            arr[link[1]].add(link[0]);
+        }
+
+        for(int i = 1 ; i <=N ; i++){
+            if(visited[i]) continue;
+            bfs(i);
+            count++;
+        }
+
+        bw.write(String.valueOf(count));
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void bfs(int root){
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(root);
+        visited[root] = true;
+
+        while(!q.isEmpty()){
+            int cur = q.poll();
+
+            for(int nxt : arr[cur]){
+                if(!visited[nxt]){
+                    q.offer(nxt);
+                    visited[nxt] = true;
+                }
+            }
+
+        }
+    }
+
+    private static int[] sToIArr(String s){
+        return Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+    
+}

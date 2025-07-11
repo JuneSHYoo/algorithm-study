3ef3aba562e49c656b5cb86c0ca69d6bdd587a7c
JuneYoo <juneyo0823@naver.com>
2025-07-11 18:11:43 +0900

    [level 3] Title: 정수 삼각형, Time: 13.64 ms, Memory: 65.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[][] d;
+    
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        int n = triangle.length;
+        d = new int[n][];
+        
+        d[0] = new int[1];
+        d[0][0] = triangle[0][0];
+        
+       
+        for(int i=1;i<n;i++){
+            int m = triangle[i].length;
+            d[i] = new int[m];
+            
+            for(int j=0; j<m ; j++){
+                if(j==0) {
+                    d[i][j] = triangle[i][j] + d[i-1][j];
+                } else if(j == m-1){
+                    d[i][j] = triangle[i][j] + d[i-1][j-1];
+                } else {
+                    d[i][j] = triangle[i][j] + Math.max(d[i-1][j] , d[i-1][j-1]);
+                }
+            }
+        }
+        
+        Arrays.sort(d[n-1]);
+        answer = d[n-1][n-1];
+        
+        return answer;
+    }
+}
